8587c8bb4d755199d8c8fcb392320781e647c2fe
Aditi <aditikurkote@gmail.com>
2025-01-13 16:48:04 +0530

    Update Voice_assistant.py

--- Voice_assistant.py
+++ Voice_assistant.py
@@ -45,7 +45,7 @@ if __name__== "__main__":
                     data1=sptext().lower()
 
                     if "your name" in data1:
-                        name = "my name is Nihal"
+                        name = "my name is Aditi"
                         speechtx(name)
 
                     elif "old are you" in data1:
